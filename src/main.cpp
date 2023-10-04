5d1a1be1827796a1a73579518a3b18f44100b710
ambiesoft <ambiesoft.trueff@gmail.com>
2023-10-04 23:45:07 +0900

    basic

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+#include "../../lsMisc/stdosd/stdosd.h"
+
+using namespace Ambiesoft::stdosd;
+using namespace std;
+
+int main()
+{
+    auto v = stdGetAllProcesses();
+    return 0;
+}
